fef4ab9d67c9be78e904ab694a198b69213496fa
Teccaue09 <00001117009051sp@al.educacao.sp.gov.br>
2024-11-28 09:49:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do nosso sistema solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do Brasil?',
+    'Rio Amazonas.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o processo pelo qual as plantas produzem alimento?',
+    'Fotossíntese.'
+)
